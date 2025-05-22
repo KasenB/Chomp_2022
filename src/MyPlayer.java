88f7989f364ea2603320b4c0e58382eae33e51ec
kasenbookbinder <kasen_bookbiner26@milton.edu>
2025-05-22 12:09:26 -0400

    started loosing boards

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -1,12 +1,16 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class MyPlayer {
     public Chip[][] gameBoard;
     public int[] columns;
+    public ArrayList<Board> winningBoards = new ArrayList<>();
+    public ArrayList<Board> loosingBoards = new ArrayList<>();
 
     public MyPlayer() {
         columns = new int[10];
 
+        loosingBoards.add(new Board(1,0,0));
         /***
          * This code will run just once, when the game opens.
          * Add your code here.
@@ -24,28 +28,41 @@ public class MyPlayer {
 
     public void oneMoveAway(int i, int j, int k) {
         // for each ijk board, print all boards that result after a single move
+        boolean foundIJKWinner = false;
 
         for (int x = k-1; x >= 0; x --){
-            System.out.println(i + " " + j + " " + x);
+            System.out.println(i + " " + j + " " + x + ": (" + x + ", 2)");
         }
 
         for (int x = j - 1; x >= 0; x--) {
             if(x>=k){
-                System.out.println(i + " " + x + " " + k);
-
+                System.out.println(i + " " + x + " " + k + ": (" + x + ", 1)");
+                for (int n = 0; n < loosingBoards.size(); n++) {
+                    if (i == loosingBoards.get(0).cols[0] && x == loosingBoards.get(n).cols[1] && k == loosingBoards.get(n).cols[2]) {
+                        foundIJKWinner = true;
+                        System.out.println("Winner winner chicken dinner");
+                    }
+                }
             } else {
-                System.out.println(i + " " + x + " " + x);
+                System.out.println(i + " " + x + " " + x + ": (" + x + ", 1)");
             }
         }
 
         for (int x = i - 1; x >0; x--){
-            if (x>=j){
-                System.out.println(x + " " + j + " " + k);
+            if (x>j && x>k) {
+                System.out.println(x + " " + j + " " + k + ": (" + x + ", 0)");
+            } else if (x>k){
+                System.out.println(x + " " + x + " " + k + ": (" + x + ", 0)");
             } else {
-                System.out.println(x + " " + x + " " + x);
+                System.out.println(x + " " + x + " " + x + ": (" + x + ", 0)");
             }
         }
 
+        if(foundIJKWinner == true){
+            winningBoards.add(new Board(i, j, k));
+        }else{
+            loosingBoards.add(new Board(i, j, k));
+        }
     }
 
 
